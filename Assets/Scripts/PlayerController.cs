94d7ef44b17fac0e6b334bf0e2badf1dedd9bf0a
William Riordan <william@Williams-MBP-4.home>
2018-07-15 19:48:44 -0400

    Created player movement system. It's not the most ideal movement but it will suit our needs for now.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+            }
+        }
+    }
 }
