0c86229ff6031817e95055f51919cb29f0a1d4a9
William Riordan <william@Williams-MacBook-Pro-4.local>
2018-07-17 11:04:11 -0400

    Completed Ability Queue System

---
+++ Assets/Scripts/AbilityHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AbilityHandler {
+    private Queue<Ability> abilities;
+
+    public AbilityHandler() {
+        abilities = new Queue<Ability>();
+    }
+
+    public void QueueAbilities() { //For development purposes only
+        Debug.Log("Queuing up abilities...");
+        abilities.enqueue(new Ability("Ability1"));
+        abilities.enqueue(new Ability("Ability2"));
+        abilities.enqueue(new Ability("Ability3"));
+
+        Debug.Log("Abilities queued.");
+    }
+
+    public void CastAbility() {
+        Debug.Log(abilities.dequeue().ToString() + " successfully casted.");
+    }
+
+}
+
+public class Ability {
+    string name;
+
+    public Ability( string _name ) {
+        name = _name;
+    }
+
+	public override string ToString()
+	{
+        return name;
+	}
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,20 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    AbilityQueue abilities = new AbilityQueue();
+    private AbilityHandler abilityHandler = new AbilityHandler();
 
-    private void Update()
+	private void Start()
+	{
+        abilityHandler.QueueAbilities();
+	}
+
+	private void Update()
+
     {
-        CastAbility(abilities.dequeue()); //for development purposes only
+        if( Input.GetKeyDown( KeyCode.A ) ) {
+            abilityHandler.CastAbility();
+        }
+
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -20,83 +29,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void queueAbilities() { //for development purposes only
-        abilities.enqueue(new Ability("Ability1"));
-        abilities.enqueue(new Ability("Ability2"));
-        abilities.enqueue(new Ability("Ability3"));
-        Debug.Log("Abilities successfully queued");
-    }
-    void CastAbility( Ability ability ) { //to be changed when abilities are made
-        if( ability == null ) {
-            return;
-        }
-
-        Debug.Log(ability.GetName() + "successfully casted.");
-    }
-}
-
-public class Ability {
-    private string name;
-
-    public Ability( string _name ) {
-        name = _name;
-    }
-    public string GetName() {
-        return name;
-    }
-}
-
-public class AbilityQueue {
-    private Ability[] abilities = new Ability[5];
-
-    public void enqueue( Ability ability ) {
-        
-            for (int i = 0; i < abilities.Length; i++)
-            {
-                if (abilities[i] != null)
-                {
-                    abilities[i] = ability;
-                    break;
-                }
-
-            }
-        //else
-        //{
-        //    Ability[] newQueue = new Ability[abilities.Length + 1];
-        //    for (int i = 0; i < abilities.Length; i++)
-        //    {
-        //        newQueue[i] = abilities[i];
-        //    }
-        //    abilities = newQueue;
-        //    abilities[abilities.Length - 1] = ability;
-        //}
-
-    }
-
-    public Ability dequeue() {
-        Ability[] newQueue = new Ability[abilities.Length];
-        Ability ret;
-
-        if ( !isEmpty() )
-        {
-            ret = abilities[0];
-            for (int i = 1; i < abilities.Length; i++)
-            {
-                newQueue[i - 1] = abilities[i];
-            }
-            return ret;
-        }
-
-        return default(Ability);
-
-    }
-
-    public bool isEmpty() {
-        if( abilities[0] == null ) {
-            return true;
-        }
-        return false;
-    }
 
 }
 
