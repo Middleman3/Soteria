641717e943cef2fc06419bb32bd7e7491bbd7ca8
William Riordan <william@Williams-MBP-4.home>
2018-07-17 20:37:41 -0400

    Working on UI system

---
+++ Assets/Scripts/GuiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiController {
+    private static GuiController instance = null;
+
+    public GuiController Instance()
+    {
+        if (instance == null)
+        {
+            instance = new GuiController();
+        }
+        return instance;
+    }
+    private GuiController()
+    {
+        
+    }
+}
